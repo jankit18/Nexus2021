7a6fc4440a2a5cac37804f776556b24e4f1571d7
Ankit Jaiswal <jankit360@gmail.com>
2021-03-31 17:17:42 +0530

    modified

--- NEXUS/settings.py
+++ NEXUS/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '4=a!ir4zy%^2=66x=rs1c#w50t1lxcxx$%a%8j!lh6-n075rpd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
